1bd5f1d2006d3847d0d05d5caaedea891a1b2dcc
Rafcik177 <92384978+Rafcik177@users.noreply.github.com>
2021-10-12 17:14:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Parafia NMP Królowej Polski w Świniarsku</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<meta name="author" content="Paweł Witowski, Rafał Michalik">
+<link rel="stylesheet" type="text/css" href="public/style/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="public/js/script.js" defer></script>
+	<script src="https://kit.fontawesome.com/029cd684fc.js" crossorigin="anonymous"></script>
+	<link rel="icon" href="public/img/parafia-logo.png">
+
+</head>
+<body>
+<div id="container">
+<div class="topbar">
+	<div class="tel">
+	tel.18 4430072
+	</div>
+	<div class="mail">
+		swiniarsko@diecezja.tarnow.pl
+	</div>
+</div>
+<nav>
+	<div class="logo">
+		<div class="logo-image">
+			<img class="logo-image-photo" src="public/img/parafia-logo.png">
+		</div>
+		<div class="name">
+			<h1>Parafia Rzymsko-Katolicka</h1>
+			<h2>Najświętszej Maryi Panny w Świniarsku</h2>
+		</div>
+		<div class="pope">
+			<img class="logo-image-photo" src="public/img/papiez.png">
+		</div>
+	</div>
+	<div class="topnav" id="myTopnav">
+		<div>
+			<a href="index.php" class="active-homepage"><i class="fas fa-home"></i></a>
+		</div>
+		<div>
+			<a href="public/views/ogloszenia.php">OGŁOSZENIA</a>
+		</div>
+		<div class="dropdown">
+			<button class="dropbtn">GALERIA
+				<i class="fa fa-caret-down"></i>
+			</button>
+			<div class="dropdown-content">
+				<a href="#">2021</a>
+				<a href="#">2020</a>
+				<a href="#">2019</a>
+			</div>
+		</div>
+		<div class="dropdown">
+			<button class="dropbtn"><a href="public/views/ogloszenia.php" style="padding:0px;">DUSZPASTERSTWO </a>
+				<i class="fa fa-caret-down"> </i>
+			</button>
+			<div class="dropdown-content">
+				<a href="public/views/duszpasterstwo.php?id=1">KANCELARIA PARAFIALNA</a>
+				<a href="public/views/duszpasterstwo.php?id=2">MSZE ŚW</a>
+				<a href="public/views/duszpasterstwo.php?id=3">NABOŻEŃSTWA</a>
+				<a href="public/views/duszpasterstwo.php?id=4">SPOWIEDŹ ŚW</a>
+				<a href="public/views/duszpasterstwo.php?id=5">CHRZTY</a>
+				<a href="public/views/duszpasterstwo.php?id=6">KSIĘŻA</a>
+				<a href="public/views/duszpasterstwo.php?id=7">GRUPY PARAFIALNE</a>
+			</div>
+		</div>
+		<div>
+			<a href="#about">KONTAKT</a>
+		</div>
+		<div>
+			<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+				<i class="fa fa-bars"></i>
+			</a>
+		</div>
+	</div>
+
+</nav>
+
+<div class="slider">
+		<img class="slider-img" src="public/img/slider-top/5e6d0d8ab4f0a_o_full.jpg">
+		<img class="slider-img" src="public/img/slider-top/unnamed.jpg">
+		<img class="slider-img" src="public/img/slider-top/IMG_20200314_112400.jpg"> 
+</div>
+
+<div class="section">
+	<div class="information">
+		<h2><strong>Msze święte w naszym kościele:</strong></h2>
+		<p><strong>- w niedzielę o godz.</strong> 7:00 *  9:00  * 11:00  * 16:00</p>
+		<p><strong>- w święta kościelne wypadające w dzień powszedni o godz.:</strong> 7:00  * 10:30  *  18:00 (17:30 czas zimowy)</p>
+		<p><strong>- w dzień powszedni od poniedziałku do soboty o godz.:</strong>  6:30  i  18:00 (17:30 czas zimowy)</p>
+		<p><strong>- w sobotę o godz. 18:00 </strong>(17:30 czas zimowy) -  Msza św. według formularza niedzielnego,  każdy kto w niej uczestniczy czyni zadość obowiązkowi uczestnictwa  w niedzielnej Mszy św.</p>
+
+		<p><strong>Odpusty:</strong></p>
+
+		<p>- ku czci NMP Królowej Polski: 3 maja</p>
+		<p>- ku czci św. Franciszka: I niedziela października</p>
+	</div>
+	<div class="slideshow-container">
+
+
+		<div class="mySlides fade" >
+
+			<img src="public/img/komunikaty/1635_jozef2.jpg" style="width:100%;">
+
+		</div>
+
+		<div class="mySlides fade">
+
+			<img src="public/img/komunikaty/5051_komunikat11.jpg" style="width:100%;">
+
+		</div> 
+
+		<div class="mySlides fade">
+
+			<img src="public/img/komunikaty/11402_synod.jpg"  style="width:100%;">
+
+		</div>
+		<!-- Next and previous buttons -->
+		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+		<a class="next" onclick="plusSlides(1)">&#10095;</a>
+		<div style="text-align:center;">
+			<span class="dot" onclick="currentSlide(1)"></span>
+			<span class="dot" onclick="currentSlide(2)"></span>
+			<span class="dot" onclick="currentSlide(3)"></span>
+		</div>
+	</div>
+
+
+</div>
+
+<article>
+	<div class="article">
+		<div class="single-notice">
+			<img src="public/img/201358_DSC_2925.jpg">
+		</div>
+		<div class="notice-name">
+			<h1>2021-01-10 Niedziela Chrztu Pańskiego</h1>
+			<p>W pierwszą niedzielę po uroczystości Objawienia Pańskiego (Trzech Króli), wspominamy chrzest Pana Jezusa w Jordanie.</p>
+		</div>
+		<div class="single-notice">
+			<img src="public/img/201358_DSC_2925.jpg">
+		</div>
+		<div class="notice-name">
+			<h1>2021-01-10 Niedziela Chrztu Pańskiego</h1>
+			<p>W pierwszą niedzielę po uroczystości Objawienia Pańskiego (Trzech Króli), wspominamy chrzest Pana Jezusa w Jordanie.</p>
+		</div>
+		<div class="single-notice">
+			<img src="public/img/201358_DSC_2925.jpg">
+		</div>
+		<div class="notice-name">
+			<h1>2021-01-10 Niedziela Chrztu Pańskiego</h1>
+			<p>W pierwszą niedzielę po uroczystości Objawienia Pańskiego (Trzech Króli), wspominamy chrzest Pana Jezusa w Jordanie.</p>
+		</div>
+		<div class="single-notice">
+			<img src="public/img/201358_DSC_2925.jpg">
+		</div>
+		<div class="notice-name">
+			<h1>2021-01-10 Niedziela Chrztu Pańskiego</h1>
+			<p>W pierwszą niedzielę po uroczystości Objawienia Pańskiego (Trzech Króli), wspominamy chrzest Pana Jezusa w Jordanie.</p>
+		</div>
+		<div class="single-notice">
+			<img src="public/img/201358_DSC_2925.jpg">
+		</div>
+		<div class="notice-name">
+			<h1>2021-01-10 Niedziela Chrztu Pańskiego</h1>
+			<p>W pierwszą niedzielę po uroczystości Objawienia Pańskiego (Trzech Króli), wspominamy chrzest Pana Jezusa w Jordanie.</p>
+		</div>
+	</div>
+	<aside>
+		<p>OGŁOSZENIA PARAFIALNE</p>
+
+		<p>1 Niedziela Wielkiego Postu - 21. 02. 2021</p>
+
+		<p>1. Droga krzyżowa w piątek o godz. 17.30 oraz w niedzielę bezpośrednio po Mszy o godz. 11.00. Gorzkie Żale z kazaniem pasyjnym w niedzielę o godz. 15.00, a bezpośrednio po nich Msza św. 15 minut przed Gorzkimi Żalami śpiew Koronki do Bożego Miłosierdzia. Zachęcamy do licznego udziału w tych nabożeństwach, zwłaszcza dzieci i młodzież lub słuchania transmisji w mediach. Za odprawienie Drogi Krzyżowej i Gorzkich Żali można zyskać odpust zupełny, pod zwykłymi warunkami. Przypominamy, że codziennie przed Mszami św. jest możliwość spowiedzi. Zachęcamy, by nie odkładać spowiedzi na same święta.
+
+
+
+		2. Bóg zapłać za ofiary składane dziś na utrzymanie Seminarium Duchownego w Tarnowie. W środę zapraszamy na Nowennę do M Bożej. Przyszła niedziela to dzień modlitw, postu i solidarności z misjonarzami. Z tej racji zbiórka do puszek będzie przeznaczona na Ogólnopolski Fundusz Misyjny. O prowadzenie różańca za tydzień przed sumą prosimy Różę św. Kingi p. Marty Lis. Bóg zapłać za ofiary składane na prace budowlane i ogrzewanie.
+
+
+
+		3. W środę po Mszy wieczornej spotkanie ministrantów i lektorów. Dziękujemy rodzinom, które sprzątały kościół i złożyły ofiarę na środki czystości. W piątek na godz. 18.30 zapraszamy kolejne rodziny ze Świniarska nry domów: 9, 522, 14, 13, 468, 578, 11.
+
+			4. Zachęcamy do czytania prasy katolickiej. Jest nowy nr Małego Gościa dla dzieci. Przez okres Wielkiego Postu wystawiony będzie pod chórem kosz Caritas, do którego jak co roku można składać trwałe produkty żywnościowe lub środki czystości, które zostaną przekazane najbardziej potrzebującym i samotnym w naszej parafii. Podobnie jak w Adwencie będą również wystawione oznakowane kosze w Lewiatanie i Centrum w Podrzeczu.</p>
+	</aside>
+</article>
+	<div class="slideshow-container-bottom">
+		<div class="mySlides-bottom fade">
+			<img src="public/img/16936_Cmentarz.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/10612_caritas3.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/10782_roze.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/1754_facebookGA.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/1754_facebookGA.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/1754_facebookGA.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/1754_facebookGA.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/10782_roze.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/10782_roze.jpg">
+		</div>
+		<div class="mySlides-bottom fade">
+			<img src="public/img/10782_roze.jpg">
+		</div>
+
+	</div>
+<footer>
+
+
+<div class="footer-box">
+	<div class="box">
+		<span class="footer-span">Polecane Linki</span>
+		<p><a href="https://profeto.pl/">profeto.pl</a></p>
+		<p><a href="https://soboty.pl/">soboty.pl</a></p>
+		<p><a href="https://diecezja.tarnow.pl/">diecezja.tarnow.pl</a></p>
+		<p><a href="https://coriss.pl/pl/">coriss.pl</a></p>
+
+	</div>
+	<div class="box">
+		<span class="footer-span">Kontakt</span>
+		<p>Świniarsko 235</p>
+		<p>33-395 Chełmiec</p>
+		<p>tel.18 4430072</p>
+		<p>swiniarsko@diecezja.tarnow.pl</p>
+	</div>
+	<div class="box">
+		<img src="public/img/kontakt.jpg">
+		<p>Konto budowy kościoła:</p>
+		<p>ŁBS   05 8805 0009 0040 0095 2000 0010</p>
+	</div>
+	
+</div>
+
+</footer>
+</div>
+</body>
+</html>
